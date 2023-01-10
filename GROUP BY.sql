2d66332da26027383a99c707bc91cba341e24470
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-10 13:17:42 +0200

    Update GROUP BY.sql

--- GROUP BY.sql
+++ GROUP BY.sql
@@ -1,16 +1,5 @@
 SELECT rating,ROUND(AVG(replacement_cost),2)
 FROM film
 GROUP BY rating
-ORDER BY round
-;
+ORDER BY round;
 
-
-
-
-SELECT rental_rate,ROUND(AVG(replacement_cost),2)
-FROM film
-GROUP BY rental_rate
-ORDER BY rental_rate
-
-
-;
