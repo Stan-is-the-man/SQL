a702ebe6ffd54797b4850a434a60c386dae5ef3d
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-11 11:00:45 +0200

    Update HAVING.sql

--- HAVING.sql
+++ HAVING.sql
@@ -22,6 +22,9 @@ GROUP BY customer_id
 HAVING SUM(amount) > 100
 ;
 
+
+Solutions to Assessment Test 1
+
 SELECT customer_id, SUM(amount)
 FROM payment
 WHERE staff_id = 2
@@ -32,4 +35,12 @@ HAVING SUM(amount) >= 110
 SELECT COUNT(title)
 FROM film
 WHERE title LIKE'J%'
+;
+
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE'E%' 
+AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1
 ;
