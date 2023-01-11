38d32a9b3231611b838be4e3ee9b15613653cd26
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-11 10:38:29 +0200

    Update HAVING.sql

--- HAVING.sql
+++ HAVING.sql
@@ -14,3 +14,10 @@ FROM payment
 GROUP BY customer_id
 HAVING COUNT(amount) > 39
 ;
+
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) > 100
+;
