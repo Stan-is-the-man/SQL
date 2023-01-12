32382623638349489b85faf0f1e593c1ce1bccb9
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-12 13:30:12 +0200

    Update LEFT JOIN.sql

--- LEFT JOIN.sql
+++ LEFT JOIN.sql
@@ -3,3 +3,11 @@ FROM film
 LEFT JOIN inventory
 ON film.film_id = inventory.film_id
 ;
+
+Or all movies that the DVD store does not have:
+SELECT film.film_id, title, inventory_id, store_id
+FROM film
+LEFT JOIN inventory
+ON film.film_id = inventory.film_id
+WHERE inventory.film_id IS null
+;
