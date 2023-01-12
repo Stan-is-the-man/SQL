39009b637f1747efbd4f895d68f7e211a097b75a
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-12 11:12:25 +0200

    Update INNER_JOIN.sql

--- INNER_JOIN.sql
+++ INNER_JOIN.sql
@@ -2,5 +2,10 @@ SELECT *
 FROM payment
 INNER JOIN customer
 ON payment.customer_id = customer.customer_id
+;
 
+SELECT payment.customer_id, payment_date, first_name, last_name, email
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id
 ;
