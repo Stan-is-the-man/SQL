c72454f12e52f6925ad2865f08005f4c2479c0ac
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-12 15:00:37 +0200

    Update LEFT JOIN.sql

--- LEFT JOIN.sql
+++ LEFT JOIN.sql
@@ -19,3 +19,11 @@ INNER JOIN customer
 ON address.address_id = customer.address_id
 WHERE district ='California'
 ;
+
+SELECT title, first_name, last_name 
+FROM film 
+INNER JOIN film_actor ON film.film_id = film_actor.film_id
+INNER JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE first_name = 'Nick' 
+AND last_name = 'Wahlberg'
+;
