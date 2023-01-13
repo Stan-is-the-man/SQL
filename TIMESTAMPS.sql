33f66284a608c0b4fb97ae91e48797e3b354b4ca
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2023-01-13 11:48:01 +0200

    Update TIMESTAMPS.sql

--- TIMESTAMPS.sql
+++ TIMESTAMPS.sql
@@ -8,3 +8,14 @@ SELECT TO_CHAR(payment_date, 'dd.mm.yyyy')
 FROM payment
 ;
 
+Assessments:
+SELECT TO_CHAR(payment_date,'MONTH')
+FROM payment
+GROUP BY TO_CHAR
+;
+
+or 
+SELECT DISTINCT(TO_CHAR(payment_date,'MONTH'))
+FROM payment
+;
+
